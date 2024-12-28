8e42de8a74428e6873a6d1c677a66bfbfa35afd5
Yuda Ferry Mahendra <yuda.mahendra@shark.tech>
2024-12-28 08:42:35 +0700

    change copilot accept and dismiss key map

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,5 +1,12 @@
 vim.keymap.set("n", "<ESC>", ":noh<CR><ESC>", { noremap = true })
 
+vim.keymap.set('i', '<C-a>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-x>', '<Plug>(copilot-dismiss)')
+
 lvim.builtin.which_key.mappings["o"] = {
   name = "Session",
   r = { "<cmd>SessionSearch<CR>", "Session search" },
